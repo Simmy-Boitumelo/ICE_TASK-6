eca4e638bca35f83c61d512f9aa6ed3b8ee7b0be
Simmy-Boitumelo <simthandilemompei22@gmail.com>
2024-09-13 20:41:51 +0200

    First Commit

---
+++ src/main/java/com/mycompany/vowels/Vowels.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vowels;
+
+/**
+ *
+ * @author Simthandile Mompei
+ */
+public class Vowels {
+
+    public static void main(String[] args) {
+       //Declare variabes
+       String word;
+       int number = 0;
+       
+       word.toLowerCase();
+       
+       for(int i = 0; i < word.length();i++){
+           number++;
+       }
+       
+        System.out.println("Number of vowels found: "+ number);
+    }
+}
